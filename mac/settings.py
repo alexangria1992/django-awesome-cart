e8b0575a53e5118f2893cc3335a9cf24e27f1368
alexangria1992 <alex040892@gmail.com>
2020-12-29 18:06:59 +1300

    models

--- mac/settings.py
+++ mac/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'shop'
+    
 ]
 
 MIDDLEWARE = [

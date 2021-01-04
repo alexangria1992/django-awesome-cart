d745826de62818e6e10666423d60f9664d02882f
alexangria1992 <alex040892@gmail.com>
2021-01-04 14:02:34 +1300

    Adding Cart PopOver & Other Pages Using Bootstrap

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
 from math import ceil
+from django.http import HttpResponse
 
 def index(request):
     # products = Product.objects.all()
@@ -23,19 +24,19 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, 'shop/search.html')
 
 def productView(request):
-    return HttpResponse("We are at productview")
+    return render(request, 'shop/prodView.html')
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request, 'shop/checkout.html')
 
 
 
